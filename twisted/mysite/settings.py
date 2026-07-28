fb35a5e36678ba97147f5502879ee7c1f82952e1
Rohan <dsairohan@gmail.com>
2026-07-28 11:56:30 +0100

    Update ALLOWED_HOSTS to allow all hosts
    Changed ALLOWED_HOSTS to allow all hosts.

--- twisted/mysite/settings.py
+++ twisted/mysite/settings.py
@@ -27,11 +27,7 @@ SECRET_KEY = 'django-insecure-&3p#whs%8@!e1=fl$!wk^y6^e@z+714@714xd$=0ghmfyk43bn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".shipwrights.dev",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
