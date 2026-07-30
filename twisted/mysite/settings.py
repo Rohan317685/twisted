57f7003639688350e9fbbb4f86ca06635084506b
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-07-30 22:06:28 +0530

    do change

--- twisted/mysite/settings.py
+++ twisted/mysite/settings.py
@@ -30,14 +30,16 @@ DEBUG = True
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 allowed_hosts_raw = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost")
 ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_raw.split(",") if host.strip()]
 
 
 csrf_origins_raw = os.getenv("CSRF_TRUSTED_ORIGINS", "http://127.0.0.1:8000,http://localhost:8000")
 CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in csrf_origins_raw.split(",") if origin.strip()]
-# Application definition
 
+# Application definition
 TAILWIND_APP_NAME = 'tailwindcsstheme'
 
 INSTALLED_APPS = [
@@ -74,8 +76,6 @@ if DEBUG:
     MIDDLEWARE += [
         "django_browser_reload.middleware.BrowserReloadMiddleware",
     ]
-
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'mysite.urls'
 
