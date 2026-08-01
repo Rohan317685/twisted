5ac4326a8271552e4f051bd8b45562f5069e0e7d
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-08-01 15:32:33 +0530

    make login disabled an option

--- twisted/twisted_site/views/client/homepage.py
+++ twisted/twisted_site/views/client/homepage.py
@@ -1,10 +1,15 @@
 from django.views import View
 from django.shortcuts import render
+import os
 
 # Create your views here.
 class HomepageView(View):
     def get(self, request):
-        return render(request, "client/homepage.html")
+        if os.environ.get("LOGIN_ENABLED") == 'false':
+            login_enabled = False
+        else:
+            login_enabled = True
+        return render(request, "client/homepage.html", {"login_enabled": login_enabled})
 
 class FaqsView(View):
     def get(self, request):
