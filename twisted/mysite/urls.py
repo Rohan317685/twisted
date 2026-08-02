1875f76edfb4929857b1d2a5a007f80c809d304a
KavyanshKhaitan2 <kavyanshkhaitan11@gmail.com>
2026-08-02 11:27:15 +0530

    hotfix

--- twisted/mysite/urls.py
+++ twisted/mysite/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("twisted_site.urls")),
     path('martor/', include('martor.urls')),
-    path('martor/uploader/', markdown_imgur_uploader, "imgur_uploader"),
+    path('martor/uploader/', markdown_imgur_uploader, name="imgur_uploader"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
