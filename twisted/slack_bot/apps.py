51a017277fd4a76732174dba5b393070d1e62d37
Aoishik Khan <73829596+aoishik@users.noreply.github.com>
2026-08-14 17:48:00 +0000

    Update socket mode handler

--- twisted/slack_bot/apps.py
+++ twisted/slack_bot/apps.py
@@ -1,5 +1,17 @@
+import os
+
 from django.apps import AppConfig
 
 
 class SlackBotConfig(AppConfig):
     name = 'slack_bot'
+
+    def ready(self):
+        super().ready()
+
+        if os.getenv("SLACK_MODE", "https").strip().lower() != "socket":
+            return
+
+        from .slack import slack_bot
+
+        slack_bot.start()
